244dc9332d742b23b731fd53182042f142eba770
Adrián Bedón <arbedon2002@gmail.com>
2023-01-31 19:53:11 -0500

    Uso de APPI para generar usernames aleatorios

--- ABMAppFinal/ABMViews/ABMRegisterUser.xaml.cs
+++ ABMAppFinal/ABMViews/ABMRegisterUser.xaml.cs
@@ -1,4 +1,5 @@
 using ABMAppFinal.ABMModels;
+using Newtonsoft.Json;
 
 namespace ABMAppFinal.ABMViews;
 
@@ -12,6 +13,11 @@ public partial class ABMRegisterUser : ContentPage
         LoadData();
 	}
 
+    protected override void OnAppearing()
+    {
+        LoadData();
+    }
+
     public void LoadData()
     {
         if (App.UserApp != null)
@@ -19,12 +25,14 @@ public partial class ABMRegisterUser : ContentPage
             ABMItemU = App.UserApp;
             imagePath = App.UserApp.abmProfilePicture;
             ABMSvBtnUser.Text = "Edit Profile";
+            this.Title = "Editar Perfil";
         }
         else
         {
             
             ABMItemU = new ABMUser();
             ABMSvBtnUser.Text = "Registrar Usuario";
+            this.Title = "Registrar Usuario";
         }
         BindingContext = ABMItemU;
     }
@@ -71,6 +79,25 @@ public partial class ABMRegisterUser : ContentPage
             await App.Current.MainPage.DisplayAlert("User", "User created!", "Ok");
 
             await Shell.Current.GoToAsync("..");
+        }
+    }
+
+    public async void OnRandomUser_Clicked(object sender, EventArgs e)
+    {
+        var request = new HttpRequestMessage();
+        request.RequestUri = new Uri("https://random-data-api.com/api/v2/users");
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json");
+
+        var client = new HttpClient();
+
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<Root>(content);
+            ABMUsername.Text = resultado.username;
         }
     }
 }
